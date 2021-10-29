1b81c841903ac700d9d71174171f29dcbfb2d4ae
Robert Zuchnicki <robert.zuchnicki@gmail.com>
2021-10-29 21:20:10 +0200

    delete getCurrentData method

--- SmartWarehouse/src/main/java/pl/infoshare/service/DataTime.java
+++ SmartWarehouse/src/main/java/pl/infoshare/service/DataTime.java
@@ -7,12 +7,6 @@ import java.time.temporal.ChronoUnit;
 
 public class DataTime {
 
-    public static String getCurrentDate() {
-        LocalDate myObj = LocalDate.now();
-        String dataString = myObj.toString();
-        return dataString;
-    }
-
     public static String getCurrentTime() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_TIME;
