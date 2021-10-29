c9ab6b3fec460d73ca7dc3e91924fd6373e12bcb
Robert Zuchnicki <robert.zuchnicki@gmail.com>
2021-10-29 21:13:31 +0200

    add JsonToObiect Class

---
+++ SmartWarehouse/src/main/java/pl/infoshare/service/JsonToObiect.java
@@ -0,0 +1,23 @@
+package pl.infoshare.service;
+
+import com.fasterxml.jackson.databind.*;
+import java.io.File;
+
+public class JsonToObiect {
+
+    public <I> I converteJsonFileToDomainObiect(Class<I> cls, String fileName, String pathway) {
+        I result = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File(pathway + fileName + ".json");
+        try {
+            result = objectMapper.readValue(file, cls);
+        } catch (Exception e) {
+            System.out.println("\nAn error occured. Pease follow the steps below:\n" +
+                    "---------------------------------------------------------------------------------------------\n" +
+                    "a) Make sure you entered the correct name of the file you are looking for\n" +
+                    "b) Make sure you have entered the file name only, without any .json extension\n" +
+                    "---------------------------------------------------------------------------------------------");
+        }
+        return result;
+    }
+}
