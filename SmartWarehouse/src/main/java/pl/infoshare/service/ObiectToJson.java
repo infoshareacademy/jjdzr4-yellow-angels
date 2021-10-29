4b35254adb35146d740168483d18d1b22d42a8c9
Robert Zuchnicki <robert.zuchnicki@gmail.com>
2021-10-29 21:12:05 +0200

    add ObiectToJson Class

---
+++ SmartWarehouse/src/main/java/pl/infoshare/service/ObiectToJson.java
@@ -0,0 +1,49 @@
+package pl.infoshare.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ObiectToJson {
+
+    public void createJSONObject(Object object, String pathway) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String simpleClassName = object.getClass().getSimpleName();
+        String fileName = DataTime.getCurrentTime();
+        if(ifSaveFile(fileName,simpleClassName)) {
+            try {
+                objectMapper.writerWithDefaultPrettyPrinter().writeValue(
+                        new FileWriter(pathway
+                                + DataTime.getCurrentTime() +
+                                ".json"), object);
+                System.out.println("CREATE FILE: '" + DataTime.getCurrentTime() + "' IN PATH: " + pathway);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Saving file aborted!");
+        }
+    }
+
+    public boolean ifSaveFile(String fileName, String className) {
+        boolean result = false;
+        System.out.println("--------------------------------\n" +
+                "Do you want to save file?\n" +
+                "Name: " + fileName + "\n" +
+                "Type: " + className + "\n" +
+                "--------------------------------\n" +
+                "a) If yes enter 'y', then press enter\n" +
+                "b) If no enter any keyboard key, then press enter");
+        Scanner scanner = new Scanner(System.in);
+        String resultYn = scanner.nextLine();
+        if(resultYn.equals("Y") || resultYn.equals("y")) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+
+}
