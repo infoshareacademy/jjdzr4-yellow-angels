218e6528c76159f35f8941e594bac326e4cfef13
Robert Zuchnicki <robert.zuchnicki@gmail.com>
2021-10-30 08:38:08 +0200

    sobota1

--- SmartWarehouse/src/main/java/pl/infoshare/App.java
+++ SmartWarehouse/src/main/java/pl/infoshare/App.java
@@ -1,6 +1,12 @@
 package pl.infoshare;
 
 
+import pl.infoshare.model.Category;
+import pl.infoshare.model.Item;
+import pl.infoshare.model.Producer;
+import pl.infoshare.service.JsonToObiect;
+import pl.infoshare.service.ObiectToJson;
+
 /**
  * Hello world!
  *
@@ -10,10 +16,25 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Yellow Angels" );
+//        System.out.println( "Yellow Angels" );
+//
+//        Menu menu = new Menu();
+//        menu.run();
 
-        Menu menu = new Menu();
-        menu.run();
+        Category category = new Category(1, "Cola");
+        Producer producer = new Producer("Pro");
+
+        Item item = new Item("Ala", category, producer);
+
+//        ObiectToJson obiectToJson = new ObiectToJson();
+//        obiectToJson.createJSONObject(item, "/home/rob/Desktop/yellowAngelsProjekt/SmartWarehouse/");
+
+        JsonToObiect jsonToObiect = new JsonToObiect();
+        Item item1 = jsonToObiect.converteJsonFileToDomainObiect(Item.class, "21:56:15", "/home/rob/Desktop/yellowAngelsProjekt/SmartWarehouse");
+        String name = item1.getCategory().getName();
+        System.out.println(name);
+
+//  /home/rob/Desktop/yellowAngelsProjekt/SmartWarehouse/21:56:15.json
     }
 
 }
