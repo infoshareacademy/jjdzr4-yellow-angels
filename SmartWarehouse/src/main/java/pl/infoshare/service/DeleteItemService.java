610070c3da1b2bebd92ca5ed8884300464f3f602
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-01 22:26:35 +0100

    add logic to deleteItemService

--- SmartWarehouse/src/main/java/pl/infoshare/service/DeleteItemService.java
+++ SmartWarehouse/src/main/java/pl/infoshare/service/DeleteItemService.java
@@ -10,7 +10,7 @@ public class DeleteItemService {
 
     private static final DataFactory factory = DataFactory.getINSTANCE;
 
-    public static void deleteItem(){
+    public static void deleteItem() {
         getItemById();
     }
 
@@ -18,18 +18,24 @@ public class DeleteItemService {
 
         System.out.println("Podaj id produktu, który chcesz usunąć: ");
         factory.getItems().forEach(System.out::println);
-        int id = ConsoleInput.getInputUserInteger() - 1;
+        int id = ConsoleInput.getInputUserInteger();
 
         List<ItemComponent> items = factory.getItems();
+        boolean notFound = true;
+        ItemComponent itemToRemove = null;
         for (ItemComponent item : items) {
             if (item.getId() == id) {
-                factory.getItems().remove(item);
+                itemToRemove = item;
+                notFound = false;
             }
         }
 
-        if (factory.getProducers().size() < id) {
+        if (notFound) {
             System.out.println("Brak wskazanego produktu.");
             getItemById();
+        } else {
+            items.remove(itemToRemove);
+            System.out.println("Usunięto produkt " + itemToRemove + ".");
         }
     }
 }
