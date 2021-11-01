1838ab445a29da10f5c805d30baf6681b6742548
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-01 10:54:36 +0100

    delete static final SCANNER

--- SmartWarehouse/src/main/java/pl/infoshare/utils/ConsoleInput.java
+++ SmartWarehouse/src/main/java/pl/infoshare/utils/ConsoleInput.java
@@ -3,60 +3,55 @@ package pl.infoshare.utils;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static java.text.ChoiceFormat.nextDouble;
-
 
 public class ConsoleInput {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
-
-
-        public static String getInputUserString() {
-            boolean inputIsNotValid = true;
-            String input = "";
-            do{
-                try {
-                    input = new Scanner(System.in).nextLine();
-                    inputIsNotValid = input.trim().isEmpty();
-                } catch (InputMismatchException e) {
-                    System.out.println("Wrong input.");
-                    inputIsNotValid = true;
-                }
-            }while (inputIsNotValid);
-            return input;
-        }
-
-    public static Double getInputUserDouble() {
-
-        boolean inputIsNotValid = true;
-        Double input = 0.0;
-        do{
+    public static String getInputUserString() {
+        boolean inputIsNotValid;
+        String input = "";
+        do {
             try {
-                input = new Scanner(System.in).nextDouble();
-                inputIsNotValid = false;
-                
+                input = new Scanner(System.in).nextLine();
+                inputIsNotValid = input.trim().isEmpty();
             } catch (InputMismatchException e) {
                 System.out.println("Wrong input.");
                 inputIsNotValid = true;
             }
-        }while (inputIsNotValid);
+        } while (inputIsNotValid);
         return input;
     }
 
-        public static Integer getInputUserInteger() {
-            boolean inputIsNotValid = true;
-            int result = 0;
-            do{
-                try {
+    public static Double getInputUserDouble() {
 
-                    result = new Scanner(System.in).nextInt();
-                    inputIsNotValid = false;
-                } catch (InputMismatchException e) {
-                    System.out.println("Wrong input.");
-                    inputIsNotValid = true;               }
-            } while (inputIsNotValid);
-            return result;
-        }
+        boolean inputIsNotValid;
+        Double input = 0.0;
+        do {
+            try {
+                input = new Scanner(System.in).nextDouble();
+                inputIsNotValid = false;
 
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input.");
+                inputIsNotValid = true;
+            }
+        } while (inputIsNotValid);
+        return input;
     }
+
+    public static Integer getInputUserInteger() {
+        boolean inputIsNotValid;
+        int result = 0;
+        do {
+            try {
+
+                result = new Scanner(System.in).nextInt();
+                inputIsNotValid = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input.");
+                inputIsNotValid = true;
+            }
+        } while (inputIsNotValid);
+        return result;
+    }
+}
 
