3e58544e7ffd43a21d56db02d137febe43470973
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-01 10:36:34 +0100

    DataFactory category and producer id from 1

--- SmartWarehouse/src/main/java/pl/infoshare/dataFactory/DataFactory.java
+++ SmartWarehouse/src/main/java/pl/infoshare/dataFactory/DataFactory.java
@@ -1,6 +1,7 @@
 package pl.infoshare.dataFactory;
 
 import pl.infoshare.model.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,28 +14,28 @@ public class DataFactory {
 
     private DataFactory() {
 
-        Category c0 = new Category(0, "category 0");
         Category c1 = new Category(1, "category 1");
         Category c2 = new Category(2, "category 2");
+        Category c3 = new Category(3, "category 3");
 
-        Producer p0 = new Producer(0, "producer 0", null);
         Producer p1 = new Producer(1, "producer 1", null);
         Producer p2 = new Producer(2, "producer 2", null);
+        Producer p3 = new Producer(3, "producer 3", null);
 
         items = new ArrayList<>(List.of(
-                new Item(1,"Item 1", c1, p1),
-                new Item(6, "Item 11", c1,p1),
-                new Item(2,"Item 2", c1, p2),
-                new Item(3,"Item 3", c2, p0),
-                new Item(4,"Item 4", c0, p1),
-                new Pack(5,"Item 5", c0, p1)));
+                new Item(1, "Item 1", c1, p1),
+                new Item(6, "Item 11", c1, p1),
+                new Item(2, "Item 2", c1, p2),
+                new Item(3, "Item 3", c2, p3),
+                new Item(4, "Item 4", c3, p1),
+                new Pack(5, "Item 5", c3, p1)));
 
         categories = new ArrayList<>(List.of(
-                c0, c1, c2
+                c3, c1, c2
         ));
 
         producers = new ArrayList<>(List.of(
-                p0, p1, p2
+                p3, p1, p2
         ));
     }
 
