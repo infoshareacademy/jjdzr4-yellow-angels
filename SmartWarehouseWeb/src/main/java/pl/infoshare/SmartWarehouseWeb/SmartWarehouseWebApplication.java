f56951a3e8c05adb12c46e3c10009ff11b66a155
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-21 13:19:17 +0100

    init project

---
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/SmartWarehouseWeb/SmartWarehouseWebApplication.java
@@ -0,0 +1,13 @@
+package pl.infoshare.SmartWarehouseWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWarehouseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWarehouseWebApplication.class, args);
+	}
+
+}
---
+++ SmartWarehouseWeb/src/test/java/pl/infoshare/SmartWarehouseWeb/SmartWarehouseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.infoshare.SmartWarehouseWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWarehouseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
