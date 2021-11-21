9afff1f7741604e4bcfc0b8543043ab7879ee3db
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-21 17:16:22 +0100

    refactor

--- SmartWarehouseWeb/src/main/java/pl/infoshare/SmartWarehouseWebApplication.java
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/SmartWarehouseWebApplication.java
@@ -1,4 +1,4 @@
-package pl.infoshare.SmartWarehouseWeb;
+package pl.infoshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SmartWarehouseWeb/src/main/java/pl/infoshare/controller/MainController.java
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/controller/MainController.java
@@ -1,4 +1,4 @@
-package pl.infoshare.SmartWarehouseWeb.controller;
+package pl.infoshare.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String main() {
         return "index";
     }
