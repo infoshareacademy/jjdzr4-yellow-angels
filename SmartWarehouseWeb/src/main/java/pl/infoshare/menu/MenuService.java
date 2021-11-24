eaa8cbfa031802e57e7fc1b38cffb07ee4fdf0bb
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-24 15:14:19 +0100

    change naming convention

--- SmartWarehouseWeb/src/main/java/pl/infoshare/menu/MenuService.java
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/menu/MenuService.java
@@ -9,10 +9,10 @@ public class MenuService {
 
     public List<MenuObject> getMenu(){
         return List.of(
-                new MenuObject(1, "Sprawdź stan magazynu", "stan_magazynu"),
-                new MenuObject(2, "Dodaj produkt", "dodaj_produkt"),
-                new MenuObject(3, "Usuń produkt", "usun_produkt"),
-                new MenuObject(4, "Edytuj produkt", "edytuj_produkt"),
+                new MenuObject(1, "Sprawdź stan magazynu", "stan-magazynu"),
+                new MenuObject(2, "Dodaj produkt", "dodaj-produkt"),
+                new MenuObject(3, "Usuń produkt", "usun-produkt"),
+                new MenuObject(4, "Edytuj produkt", "edytuj-produkt"),
                 new MenuObject(5, "Wyszukiwarka", "wyszukiwarka"));
     }
 }
