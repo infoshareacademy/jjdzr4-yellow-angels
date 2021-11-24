4f89abe85e40df1e732ce7748887f30a08838e18
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-11-24 18:03:01 +0100

    add classes form console version

---
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/model/Producer.java
@@ -0,0 +1,37 @@
+package pl.infoshare.model;
+
+public class Producer {
+    private int id;
+    public String name;
+    private Address address;
+
+    public Producer(String name) {
+        this.name = name;
+    }
+
+    public Producer(int id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +
+                id +
+                ".) " +
+                name;
+    }
+}
