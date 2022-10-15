560f0680fa427f4f13c1c9b793ab131a8da2e1a5
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-10-15 12:21:39 +0200

    translate to english

--- SmartWarehouseWeb/src/main/java/pl/infoshare/service/MenuObjectService.java
+++ SmartWarehouseWeb/src/main/java/pl/infoshare/service/MenuObjectService.java
@@ -10,11 +10,9 @@ public class MenuObjectService {
 
     public List<MenuObject> getMenu(){
         return List.of(
-                new MenuObject(1, "Stan magazynu", "stock"),
-                new MenuObject(2, "Lista produktów", "products"),
-                new MenuObject(3, "Dodaj produkt", "add-product"),
-                new MenuObject(4, "Usuń produkt", "delete-product"),
-                new MenuObject(5, "Edytuj produkt", "edit-product"),
-                new MenuObject(6, "Wyszukiwarka", "search-engine"));
+                new MenuObject(1, "Stock status", "stock"),
+                new MenuObject(2, "Products", "products"),
+                new MenuObject(3, "Add product", "add-product"),
+                new MenuObject(4, "Search", "search-engine"));
     }
 }
